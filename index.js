166863a42b49f0a4637ab8c92ec5b306194d76c3
Igor Fedyukin <igor_fedyukin@epam.com>
2018-01-19 12:30:56 +0300

    Init

---
+++ index.js
@@ -0,0 +1,15 @@
+const TelegramBot = require('node-telegram-bot-api');
+
+const token = process.env.BOT_TOKEN;
+const bot = new TelegramBot(token, {polling: true});
+
+bot.onText(/\/echo (.+)/, (msg, match) => {
+  const chatId = msg.chat.id;
+  const resp = match[1];
+  bot.sendMessage(chatId, resp);
+});
+
+bot.on('message', (msg) => {
+  const chatId = msg.chat.id;
+  bot.sendMessage(chatId, 'Received your message');
+});
